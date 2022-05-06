734c93d8f07c3e9af6fdfe609c02074a8beff093
Gabriel Lazaroni <67127765+GabrielLazaroni@users.noreply.github.com>
2022-05-05 22:25:34 -0300

    add new templates and macro

--- myfood/ext/site/main.py
+++ myfood/ext/site/main.py
@@ -1,3 +1,4 @@
+from re import template
 from flask import render_template 
 from flask import Blueprint, render_template
 
@@ -7,3 +8,11 @@ bp = Blueprint('site', __name__)
 @bp.route('/')
 def index():
   return render_template('index.html')
+
+@bp.route('/sobre')
+def about():
+  return render_template('about.html')
+
+@bp.route('/restaurantes')
+def restaurants():
+  return render_template('restaurants.html')
