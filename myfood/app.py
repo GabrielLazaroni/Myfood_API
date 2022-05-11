84a829a70a887e069a0f4135a7b48b46b9b9235a
Gabriel Lazaroni <gabriellazaroni@outlook.com.br>
2022-05-10 23:18:54 -0300

    some adjustments

--- myfood/app.py
+++ myfood/app.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from myfood.ext import site
+from myfood.ext import config
+from myfood.ext import toolbar
+
 
 def create_app():
   app = Flask(__name__)
+  config.init_app(app)
   site.init_app(app)
+  toolbar.init_app(app)
   return app
---
+++ myfood/ext/config/__init__.py
@@ -0,0 +1,2 @@
+def init_app(app):
+    app.congig['SECRET_KEY'] = '123456'
